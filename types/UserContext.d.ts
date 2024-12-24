67b1ca4252c50cd9f4e72d1f2315b95fa345e48c
jometa <joaquinmetayer@gmail.com>
2024-12-24 08:48:53 -0300

    backend boilerplate

--- types/UserContext.d.ts
+++
@@ -1,9 +0,0 @@
-export interface UserContextProps {
-    walletAddress: string | null;
-    isAuthenticated: boolean;
-    userRole: string | null; // Ejemplo: rol del usuario (admin, usuario, etc.)
-    setWalletAddress: (address: string | null) => void;
-    setUserRole: (role: string) => void;
-    logout: () => void;
-  }
-  
